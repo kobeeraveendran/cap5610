f0d87e8d6e04e1f12235b1a6c219a5ec9e8a401e
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2019-09-13 15:21:21 -0400

    added part of dataset creation

--- assignments/assignment1/dataset_preprocessing.py
+++ assignments/assignment1/dataset_preprocessing.py
@@ -2,18 +2,19 @@ import csv
 import numpy as np
 
 def load_dataset():
-    with open('iris.data', 'rb') as file:
+    with open('dataset/iris.data', 'rb') as file:
         lines = file.readlines()
 
     dataset = [[]]
 
     for i in range(len(lines)):
-        new_line = lines[i].split(',')
+
+        new_line = str(lines[i]).split(',')
 
         features = new_line[:-1]
         class_label = new_line[-1]
 
-        class_label = class_label.split('-')[1]
+        class_label = class_label.split('-')[1][:-3]
         
         # setosa = 0, versicolour = 1, virginica = 2
         if class_label == 'setosa':
@@ -25,7 +26,19 @@ def load_dataset():
         else:
             class_label = 2
 
-        dataset[i] = features
-        dataset[i].append(class_label)
+        dataset.append([features, class_label])
+        #dataset[i].append(class_label)
 
     return dataset
+
+# testing
+dataset = load_dataset()
+
+for i in range(len(dataset)):
+    for j in range(len(dataset[0])):
+        print(dataset[i][j])
+
+def generate_confusion_matrix(predicted_class, actual_class):
+    confusion_matrix = pd.df()
+
+    return confusion_matrix
---
+++ assignments/assignment1/knn_3819943.py
@@ -0,0 +1,4 @@
+import numpy as np
+import math
+
+def euclidean_distance()
