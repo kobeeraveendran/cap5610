b43918066f880df3aad93780c2cf4293e3c5c7e9
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2019-09-16 15:45:15 -0400

    added argparser

--- assignments/assignment1/knn_3819943.py
+++ assignments/assignment1/knn_3819943.py
@@ -1,4 +1,19 @@
 import numpy as np
 import math
+import argparse
 
-def euclidean_distance()
+parser = argparse.ArgumentParser()
+parser.add_argument('-k', type = int, nargs = 1, action = 'store', dest = 'k')
+parser.add_argument('-m', type = str, nargs = 1, action = 'store', dest = 'mode')
+
+dist_metric = 'Euclidean'
+
+def euclidean_distance(p1, p2):
+
+    e_dist = 0
+
+    return e_dist
+
+
+def cosine_distance(p1, p2):
+    
