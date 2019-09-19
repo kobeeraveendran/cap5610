cc9464361e3998dc5a5ccca68ed96624f07ea8c7
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2019-09-19 11:53:01 -0400

    added temp cosine similarity metric

--- assignments/assignment1/knn_3819943.py
+++ assignments/assignment1/knn_3819943.py
@@ -10,10 +10,16 @@ dist_metric = 'Euclidean'
 
 def euclidean_distance(p1, p2):
 
-    e_dist = 0
+    e_dist = np.sqrt(np.sum((p1 - p2) ** 2))
 
     return e_dist
 
 
 def cosine_distance(p1, p2):
     
+    # check this later
+    cos_similarity = (np.sum(p1 * p2)) / (np.sqrt(np.sum(p1 ** 2)) * np.sqrt(np.sum(p2 ** 2)))
+
+    cos_dist = 1 - cos_similarity
+
+    return cos_dist
