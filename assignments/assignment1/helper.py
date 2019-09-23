af77f6cf3c86b75082f8bbff51a2dd5012cdabc7
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2019-09-22 20:52:52 -0400

    added confusion matrix generation (mpl and seaborn)

--- assignments/assignment1/helper.py
+++ assignments/assignment1/helper.py
@@ -1,6 +1,9 @@
 import csv
 import numpy as np
 import random
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def load_dataset():
     with open('iris.data', 'r') as file:
@@ -48,9 +51,58 @@ def train_test_split(dataset, split_ratio):
 
     return training_set, test_set
 
+def generate_confusion_matrix(actual, predicted, plot_title, num_classes = 3):
+    confusion_matrix = np.zeros((num_classes, num_classes), dtype = int)
+
+    for i in range(len(actual)):
+        confusion_matrix[actual[i]][predicted[i]] += 1
+
+    print(confusion_matrix)
+
+    # optional heatmap for better visual idea
+    
+    # ask if seaborn is allowed
+    #plt.figure(figsize = (10, 7))
+    #sns.set(font_scale = 1.4)
+    #sns.heatmap(pd.DataFrame(confusion_matrix), annot = True)
+    #plt.show()
+
+    # using matplotlib only
+    fig, ax = plt.subplots()
+    im = ax.imshow(confusion_matrix, interpolation = 'nearest', cmap = plt.cm.Blues)
+    ax.figure.colorbar(im, ax = ax)
+    ax.set(xticks = np.arange(confusion_matrix.shape[0]), 
+           yticks = np.arange(confusion_matrix.shape[1]), 
+           xticklabels = ['setosa', 'versicolor', 'virginica'], 
+           yticklabels = ['setosa', 'versicolor', 'virginica'], 
+           xlabel = 'Predicted class', 
+           ylabel = 'True class')
+
+    plt.title(plot_title, y = 1.12)
+
+    ax.xaxis.set_label_position('top')
+    ax.xaxis.tick_top()
+
+    plt.setp(ax.get_xticklabels(), ha = 'center', rotation_mode = 'anchor')
+
+    threshold = confusion_matrix.max() / 2
+
+    for i in range(confusion_matrix.shape[0]):
+        for j in range(confusion_matrix.shape[1]):
+            ax.text(j, i, format(confusion_matrix[i, j], 'd'), ha = 'center', va = 'center', 
+            color = 'white' if confusion_matrix[i, j] > threshold else 'black')
+
+    fig.tight_layout()
+
+    return ax
+
 #training_set, test_set = train_test_split(dataset, 0.7)
 
 #print('Training set size: ', len(training_set))
 #print('Test set size: ', len(test_set))
 
 #print(generate_k_folds(dataset))
+
+plot = generate_confusion_matrix([2, 0, 2, 2, 0, 1], [0, 0, 2, 2, 0, 2], 'Test confusion matrix')
+
+plt.show(plot)
