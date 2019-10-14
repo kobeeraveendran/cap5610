e843a28ac53e62e098d5dfb288da29d56e444491
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2019-10-14 01:00:27 -0400

    added completed step 1 (without avg accuracy across folds)

--- assignments/assignment2/code/svm.py
+++ assignments/assignment2/code/svm.py
@@ -1,6 +1,7 @@
 import csv
 from sklearn import svm
 import random
+import itertools
 
 def load_dataset():
     with open('../Data/glass.data') as f:
@@ -18,7 +19,6 @@ def load_dataset():
     return dataset
 
 dataset = load_dataset()
-
 
 def generate_k_folds(dataset, k = 5):
     random.shuffle(dataset)
@@ -67,8 +67,66 @@ def train_svm(train, test, kernel_type, class_weights = None, decision_fn_shape 
 
     params = clf.get_params()
 
-train = dataset[: int(0.8 * len(dataset))]
-test = dataset[int(0.8 * len(dataset)):]
+def svm_clf(train, test, kernel, degree = 3, gamma = 'auto', class_weights = None, dec_fn_shape = 'ovo'):
+    
+    if kernel == 'poly':
+        print('\n\nUsing kernel POLY with gamma = {} and degree = {}'.format(gamma, degree))
 
-#print('Training set: \n', train)
-#print('\n\nTest set: \n', test)
+    else:
+        print('\n\nUsing kernel {} with gamma = {}'.format(kernel.upper(), gamma))
+    
+    X_train, Y_train = train
+    X_test, Y_test = test
+
+    clf = svm.SVC(kernel = kernel, degree = degree, class_weight = class_weights, decision_function_shape = dec_fn_shape)
+    clf.fit(X_train, Y_train)
+
+    preds = clf.predict(X_test)
+
+    #print('Predictions: \n', preds)
+    #print('\n\nY_test: \n', Y_test)
+
+    correct = 0
+
+    for i in range(len(Y_test)):
+        if Y_test[i] == preds[i]:
+            correct += 1
+
+    print('Accuracy: ', correct / len(Y_test))
+
+
+def kernel_testing(dataset):
+    folds = generate_k_folds(dataset, k = 5)
+
+    for i, fold in enumerate(folds):
+
+        training_set = list(itertools.chain.from_iterable(folds[:i] +  folds[i + 1:]))
+        test_set = fold
+
+        X_train = [x[:-1] for x in training_set]
+        Y_train = [y[-1] for y in training_set]
+
+        X_test = [x[:-1] for x in test_set]
+        Y_test = [y[-1] for y in test_set]
+
+        train = (X_train, Y_train)
+        test = (X_test, Y_test)
+
+        '''
+        params = { 
+            'degree': [3, 4, 5], 
+            'gamma': ['auto', 'scale']
+        }
+        '''
+
+        for kernel in ['rbf', 'linear', 'poly', 'sigmoid']:
+            
+            if kernel == 'poly':
+                for degree in [3, 4, 5]:
+                    svm_clf(train, test, kernel = kernel, degree = degree)
+
+            for gamma in ['auto', 'scale']:
+                svm_clf(train, test, kernel = kernel, gamma = gamma)
+
+
+kernel_testing(dataset)
